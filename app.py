58fa976f50a389ee1a4547d19c263c1f336a047a
KUYATE UMESH SHIVAJI <202401080042@mitaoe.ac.in>
2025-04-21 01:17:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def generate_code():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
