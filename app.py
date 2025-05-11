13bbb41ccf1f5ac4ee3c29f9bc93f0c05051c512
KUYATE UMESH SHIVAJI <202401080042@mitaoe.ac.in>
2025-05-11 09:47:29 +0000

    comment added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import logging
-
+# all fine
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
